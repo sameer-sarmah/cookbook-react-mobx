0736a3c00f4b18d7641fe695274031e603ca4354
Sameer Sarmah <sameerparansarmah@gmail.com>
2018-12-03 14:24:18 +0530

    babel rc to babel config

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = function(api) {
+    api.cache(false);
+  const presets = ["mobx", "@babel/env", "@babel/react"];
+  const plugins = [
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    "@babel/plugin-transform-async-to-generator",
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-syntax-dynamic-import"
+  ];
+
+  return {
+    presets,
+    plugins
+  };
+};
