8bff4fa01b9af1a9d12658b4b2790bde9f632eaf
Sameer Sarmah <sameerparansarmah@gmail.com>
2018-12-03 12:08:39 +0530

    jest with babel 7

--- src/recipes/recipe-list/recipe-list.component.js
+++ src/recipes/recipe-list/recipe-list.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import './recipe-list.component.css';
 import PropTypes from "prop-types";
+import ReactTooltip from 'react-tooltip';
 
 export class RecipeList extends Component {
 
@@ -10,7 +11,13 @@ export class RecipeList extends Component {
     };
 
     createRow(recipeItem) {
-        const rows = (<li key={recipeItem.recipe_id} onClick={this.listItemClicked.bind(this,recipeItem)}>{recipeItem["title"]}</li>);
+    
+        const rows = (<div>
+        <li data-tip={recipeItem["social_rank"]} key={recipeItem.recipe_id} onClick={this.listItemClicked.bind(this,recipeItem)}>
+        {recipeItem["title"]}
+        </li>
+        <ReactTooltip place="right" type="light" effect="solid" getContent={(dataTip) => ` ${dataTip}`}/>
+        </div>);
         return rows;
     };
 
